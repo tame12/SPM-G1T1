462c94ff8116ba4790cc72ffe48e770678e09b75
andreayup <andreayupp@gmail.com>
2022-10-05 19:20:07 +0800

    assign_skill_to_role working

--- backend/server/flaskServer.py
+++ backend/server/flaskServer.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{DB_USERNAME}:{DB_PASSWORD}@localhost:3306/is212_g1t1'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100,
-                                           'pool_recycle': 280}
+                                        'pool_recycle': 280}
 
 db = SQLAlchemy(app)
 CORS(app)
